99e0ed1f8ca3cc9dec2cee702d0a9ef5c13f124f
zamaletdionv-a <arhimed1945@yandex.ru>
2016-08-03 15:55:42 +0300

    first iteration: Fizz

---
+++ FizzBuzz.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+const int N=100;
+
+int main()
+{
+	int mass[N];
+
+	for (int i=0; i<N; i++)
+	{
+		mass[i]=i+1;
+		if (mass[i]%3==0)
+			printf("Fizz");
+		else
+			printf("%d",mass[i]);
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return 0;
+}
